cb6fc1f71794c77aeb68c25b9adcceb72b0a323c
William Pimentel <williamencabopimentel@gmail.com>
2025-10-09 03:14:23 +0100

    Outputs each square sequentially

--- main.c
+++ main.c
@@ -9,8 +9,16 @@ struct board {
     char *grid[ROWS][COLS];
 };
 
+void display_grid(char *grid[ROWS][COLS]) {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%s\n", grid[i][j]); // actually outputs the contents of each square
+        }
+    }
+}
+
 int main(void) {
-    struct board chess_board = { // creates a structure variable (board) called chessBoard
+    struct board chess_board = { // creates a structure variable (board) called chess_board
         .grid = {
                 {"br", "bn", "bb", "bq", "bk", "bb", "bn", "br"},
                 {"bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"},
@@ -23,12 +31,12 @@ int main(void) {
                 }
     };
     
-    int count = 1;
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            printf("%s\n", chess_board.grid[i][j]); // actually outputs the contents of each square
-        }
-    }
+    // for (int i = 0; i < ROWS; i++) {
+    //     for (int j = 0; j < COLS; j++) {
+    //         printf("%s\n", chess_board.grid[i][j]); // actually outputs the contents of each square
+    //     }
+    // }
+    display_grid(chess_board.grid);
 
     return 0;
 }
