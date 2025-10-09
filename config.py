de42aef266623a4b4c6376b926da37103ff972d7
William Pimentel <williamencabopimentel@gmail.com>
2025-10-09 03:10:21 +0100

    Added helper function

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 # === Board & Layout ===
 SQUARE_SIZE = 100
 WIDTH, HEIGHT = 800, 800
+
+
+def board_to_screen(position):
+    screen_X = position[0] * SQUARE_SIZE
+    screen_Y = position[1] * SQUARE_SIZE
+    return (screen_X, screen_Y)
