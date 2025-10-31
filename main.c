f69e10a6ba6752cfc676d763fb1b22e2c6643c01
William Pimentel <williamencabopimentel@gmail.com>
2025-10-31 00:46:25 +0000

    Added temporary basic input handling

--- main.c
+++ main.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include "board.h"
 #include "piece.h"
 #include "move.h"
 
 // declare functions
-void process_input(Board*);
+void process_input(Board *board, char[], char[]);
 
 int main(void) {
     Board chess_board = init_pieces();
 
-    display_grid(&chess_board); // display chess board at the start before taking inputs
+    bool running = true;
+    char m1[] = "e2", m2[] = "e4";
 
-    make_move(&chess_board, "e2", "e4");
-
-    display_grid(&chess_board);
-
-    make_move(&chess_board, "e7", "e5");
-
-    display_grid(&chess_board);
+    while (running) {
+        display_grid(&chess_board);
+        make_move(&chess_board, m1, m2);
+        process_input(&chess_board, m1, m2);
+    }
 
     return 0;
 }
 
 // define functions
-void process_input(Board *board) {
-    char move[6]; // longest input could be a 6-char pawn capture promotion (e.g. fxg8=Q)
+void process_input(Board *board, char m1[], char m2[]) {
+    // char move[6]; // longest input could be a 6-char pawn capture promotion (e.g. fxg8=Q)
     // need to add a function that reads the move (an algebraic move parser)
     printf("Enter a move: ");
-    scanf("%s", move);
-    display_grid(board);
+    if (scanf("%2s %2s", m1, m2) != 2) {
+        fprintf(stderr, "invalid input");
+        exit(1);
+    }
 }
