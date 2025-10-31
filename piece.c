3f9f3ae8287b3d07058c078f8c1cd35a6cbe8e48
William Pimentel <williamencabopimentel@gmail.com>
2025-10-31 08:00:50 +0000

    Added queen, rook and bishop movement logic

--- piece.c
+++ piece.c
@@ -9,12 +9,12 @@ int generate_pseudo_legal_moves(Board *b, Piece *p, Position *moves) { // p is t
         return generate_knight_moves(b, p, moves, capacity);
     if (p->type == 'k')
         return generate_king_moves(b, p, moves, capacity);
-    // if (p->type == 'r' || p->type == 'b' || p->type == 'q')
-    //     return generate_sliding_moves();
-    // if (p->type == 'n')
-    //     return generate_knight_moves();
-    // if (p->type == 'k')
-    //     return generate_king_moves();
+    if (p->type == 'r')
+        return generate_rook_moves(b, p, moves, capacity);
+    if (p->type == 'b')
+        return generate_bishop_moves(b, p, moves, capacity);
+    if (p->type == 'q')
+        return generate_queen_moves(b, p, moves, capacity);
 
 }
 
@@ -100,8 +100,49 @@ int generate_king_moves(Board *b, Piece *p, Position *moves, int capacity) {
         }
     }
     return count;
-
 }
+
+int generate_sliding_moves(Board *b, Piece *p, Position *moves, int capacity, int directions[][2], int num_d) {
+    int count = 0;
+    for (int i = 0; i < num_d; i++) {
+        Position t = {p->position[0] + directions[i][0], p->position[1] + directions[i][1]}; // target square
+        Piece *target = get_piece_at(b, (int[2]){t.row, t.col}); 
+        while (in_bounds(t.row, t.col) && target->type == '\0') {
+            if (count >= capacity) break;
+            moves[count] = t;
+            count++;
+
+            t.row += directions[i][0];
+            t.col += directions[i][1];
+            target = get_piece_at(b, (int[2]){t.row, t.col}); // switch to next potential target
+        }
+    }
+    return count;
+}
+
+int generate_bishop_moves(Board *b, Piece *p, Position *moves, int capacity) {
+    int dir[4][2] = {
+        {1, 1}, {-1, 1},
+        {-1, -1}, {1, -1}
+    };
+    return generate_sliding_moves(b, p, moves, capacity, dir, 4);
+}
+
+int generate_rook_moves(Board *b, Piece *p, Position *moves, int capacity) {
+    int dir[4][2] = {
+        {1, 0}, {0, 1},
+        {-1, 0}, {0, -1}
+    };
+    return generate_sliding_moves(b, p, moves, capacity, dir, 4);
+}
+int generate_queen_moves(Board *b, Piece *p, Position *moves, int capacity) {
+    int dir[8][2] = {
+            {1, 1}, {-1, 1}, {-1, -1}, {1, -1},
+            {1, 0}, {0, 1}, {-1, 0}, {0, -1}
+    };
+    return generate_sliding_moves(b, p, moves, capacity, dir, 8);
+}
+
 
 int is_enemy(Board *board, Piece *piece, Position destination) {
     int raw_destination[2] = {destination.row, destination.col};
--- piece.h
+++ piece.h
@@ -11,7 +11,10 @@ int generate_pseudo_legal_moves(Board *b, Piece *p, Position *moves);
 int generate_pawn_moves(Board *b, Piece *p, Position *moves, int capacity);
 int generate_knight_moves(Board *b, Piece *p, Position *moves, int capacity);
 int generate_king_moves(Board *b, Piece *p, Position *moves, int capacity);
-// void generate_sliding_moves(void); // helper to simpify logic
+int generate_sliding_moves(Board *b, Piece *p, Position *moves, int capacity, int directions[][2], int num_d); // helper to simpify logic
+int generate_bishop_moves(Board *b, Piece *p, Position *moves, int capacity);
+int generate_rook_moves(Board *b, Piece *p, Position *moves, int capacity);
+int generate_queen_moves(Board *b, Piece *p, Position *moves, int capacity);
 
 // helper functions
 int is_enemy(Board *board, Piece *piece, Position destination);
