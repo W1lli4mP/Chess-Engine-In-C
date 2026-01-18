8af6ece27e1658b3212a2c0ccfb6c62e48e06851
William Pimentel <williamencabopimentel@gmail.com>
2026-01-18 01:58:16 +0000

    Add move.c

---
+++ move.c
@@ -0,0 +1,31 @@
+#include "move.h"
+
+// sets default values
+Move *initialise_move()
+{
+    Move *move = malloc(sizeof *move);
+    Position from = { .col = -1, .row = -1 };
+    Position to = { .col = -1, .row = -1 };
+
+    move->piece = TYPE_NONE;
+    move->from = from;
+    move->to = to;
+    move->promotion = TYPE_NONE;
+    move->is_capture = false;
+    move->is_castle_kingside = false;
+    move->is_castle_queenside = false;
+    move->is_check = false;
+    move->is_checkmate = false;
+    move->is_promotion = false;
+
+    return move;
+}
+
+bool destroy_move(Move *move)
+{
+    if (!move) return false;
+
+    free(move);
+
+    return true;
+}
