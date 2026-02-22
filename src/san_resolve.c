229475599fab36efafb1c9eced38132ab0b01172
William Pimentel <williamencabopimentel@gmail.com>
2026-02-22 11:25:23 +0000

    Refactor and take account for captures

--- src/san_resolve.c
+++ src/san_resolve.c
@@ -9,8 +9,18 @@ handle is capture
 */
 ResolveStatus resolve_san(const Board *board, San san, Move *move_out)
 {
+    /*
+        pipeline:
+        - iterate through all pieces
+        - check if SAN move is in the list of valid moves
+        - if SAN move is not an ordinary move (captures, castling, promotions), check for them too
+
+        handling captures:
+        - must be a legal move
+        - enemy piece must be on the SAN's destination
+    */
+
     ResolveStatus current_status = RESOLVE_ILLEGAL;
-    PieceType piece_type_target = san.piece;
     for (int row = 0; row < board->height; row++)
     {
         for (int col = 0; col < board->width; col++)
@@ -19,43 +29,47 @@ ResolveStatus resolve_san(const Board *board, San san, Move *move_out)
             if (san.from_col != -1 && san.from_col != col) continue;
             if (san.from_row != -1 && san.from_row != row) continue;
 
-            // iterate through all positions on the board
-            Position current_position = { .row = row, .col = col };
+            // iterate through all squares on the board
+            Position from = { .row = row, .col = col };
             
-            // find piece at current observed position
-            Piece *selected_piece = get_piece_at(board, current_position);
-            
-            // skip if selected piece does not exist or wrong type of piece
-            if (!selected_piece || selected_piece->type != piece_type_target) continue;
+            // find valid piece at current square
+            Piece *selected_piece = get_piece_at(board, from);
+            if (!selected_piece || selected_piece->type != san.piece) continue;
 
-            // otherwise, check if the selected piece can go to the SAN's desired destination
+            // check if the selected piece can go to the SAN's square
             PositionList move_list = {0}; // set count=0 and moves to be 0
-            if (!generate_legal_moves(board, current_position, &move_list)) continue;
+            if (!generate_legal_moves(board, from, &move_list)) continue;
 
-            // check if the SAN's desired move is in the list of possible moves
+            // check if the SAN's move is in the list of all possible moves
             for (int i = 0; i < move_list.count; i++)
             {
-                // if there's a match, break
-                if (san.to.row == move_list.moves[i].row && san.to.col == move_list.moves[i].col)
+                // select destination square
+                Position to = move_list.moves[i];
+
+                // if there's a match, process it
+                if (san.to.row == to.row && san.to.col == to.col)
                 {
-                    // TEMPORARY: link from square
-                    move_out->from = current_position;
+                    // handle captures
+                    if (san.is_capture)
+                    {
+                        // don't process move if there is no valid piece (enemy) to capture
+                        Piece *enemy = get_piece_at(board, to);
+                        if (!enemy) continue;
+                    }
 
                     // if the status has previously been marked as OK, then there must be more than one piece that can access this move
-                    if (current_status == RESOLVE_OK)
-                    {
-                        current_status = RESOLVE_AMBIGUOUS;
-                        break;
-                    }
+                    if (current_status == RESOLVE_OK) return RESOLVE_AMBIGUOUS;
+                    
+                    // approve status and update move's source square
                     current_status = RESOLVE_OK;
-                    break;
+                    move_out->from = from;
+                    continue;
                 }
             }
         }
     }
     move_out->piece = san.piece;
     move_out->to = san.to;
-    // move_out->from = ??;
 
     return current_status;
 }
