94098ac68027e1d1f2f655af92090f166ee35ff2
William Pimentel <williamencabopimentel@gmail.com>
2026-03-12 01:36:29 +0000

    Refactor API and fix promotion logic

--- src/san_resolve.c
+++ src/san_resolve.c
@@ -1,5 +1,16 @@
 #include "san_resolve.h"
 
+/*
+TODO:
+handle castling
+handle san suffix (checks/mates)
+handle turns
+*/
+static bool is_pawn_promotion_rank(const Piece *piece, Position to);
+static bool verify_square(const Board *board, San san, Position from, Piece **piece_out);
+static ResolveStatus validate_moves(const Board *board, San san, Piece *current_piece, PositionList move_list, Move *move_out);
+static ResolveStatus validate_move(const Board *board, San san, Piece *current_piece, Position to, Move *move_out);
+
 // helper for implicit pawn promotions
 static bool is_pawn_promotion_rank(const Piece *piece, Position to)
 {
@@ -9,87 +20,136 @@ static bool is_pawn_promotion_rank(const Piece *piece, Position to)
     return false;
 }
 
-/*
-TODO:
-handle castling
-handle promotions
-handle san suffix (checks/mates)
-*/
+
+// verifies ONE square
+static bool verify_square(const Board *board, San san, Position from, Piece **piece_out)
+{
+    // disambiguation verification
+    if (san.from_col != -1 && san.from_col != from.col) return false;
+    if (san.from_row != -1 && san.from_row != from.row) return false;
+
+    // piece and piece type verification
+    Piece *current_piece = get_piece_at(board, from);
+
+    if (!current_piece) return false;
+    if (current_piece->type != san.piece) return false;
+
+    // extract piece
+    *piece_out = current_piece;
+    return true;
+}
+
+// populates a Move struct while verifying
+// particularly: to
+static ResolveStatus validate_moves(const Board *board, San san, Piece *current_piece, PositionList move_list, Move *move_out)
+{
+    bool found = false;
+
+    // validate if any moves match the SAN (to)
+    for (int i = 0; i < move_list.count; i++)
+    {
+        Position to = move_list.moves[i];
+        if (to.col != san.to.col || to.row != san.to.row) continue;
+
+        //* validate if the move match the SAN's other attributes
+        ResolveStatus current_status = validate_move(board, san, current_piece, to, move_out);
+        if (current_status != RESOLVE_OK) continue;
+
+        // cannot have repeats
+        if (found) return RESOLVE_AMBIGUOUS;
+
+        move_out->to = to;
+        found = true;
+    }
+    return found ? RESOLVE_OK : RESOLVE_ILLEGAL;
+}
+
+// helper that verifies a singular valid move based on SAN intent
+// also populates a Move struct while verifying
+// particularly: is_promotion, promotion
+static ResolveStatus validate_move(const Board *board, San san, Piece *current_piece, Position to, Move *move_out)
+{
+    // captures (target can be an enemy or ally)
+    Piece *target = get_piece_at(board, to);
+    if (san.is_capture)
+    {
+        if (!target) return RESOLVE_ILLEGAL;
+        if (target->colour == current_piece->colour) return RESOLVE_ILLEGAL;
+    }
+    else if (target) return RESOLVE_ILLEGAL;
+
+    // explicit and implicit promotions
+    if (san.is_promotion || is_pawn_promotion_rank(current_piece, to))
+    {
+        if (!is_pawn_promotion_rank(current_piece, to)) return RESOLVE_ILLEGAL;
+
+        // default to queen if no promotion piece is explicitly given
+        move_out->is_promotion = true;
+        move_out->promotion = (san.promotion == TYPE_NONE) ? TYPE_QUEEN : san.promotion;
+    }
+
+    return RESOLVE_OK;
+}
+
 ResolveStatus resolve_san(const Board *board, San san, Move *move_out)
 {
     /*
-        pipeline:
-        - iterate through all pieces
-        - check if SAN move is in the list of valid moves
-        - if SAN move is not an ordinary move (captures, castling, promotions), check for them too
-
-        handling captures:
-        - must be a legal move
-        - enemy piece must be on the SAN's destination
+        purpose:
+        verify if the input SAN is a illegal, ambigious or ok
+        populates a Move struct iff ok
     */
 
-    ResolveStatus current_status = RESOLVE_ILLEGAL;
+    // default status to illegal
+    ResolveStatus status = RESOLVE_ILLEGAL;
+
+    // updates move_out safely
+    Move found_move;
+
+    bool found = false;
+
     for (int row = 0; row < board->height; row++)
     {
         for (int col = 0; col < board->width; col++)
         {
-            // check for disambiguation hints from SAN
-            if (san.from_col != -1 && san.from_col != col) continue;
-            if (san.from_row != -1 && san.from_row != row) continue;
+            //? how it works: finds a move that satisfies the SAN, if there is more than one move, resolve as ambiguous, else populate move_out
+            // verify square and extract piece if valid
+            Position from = { .col = col, .row = row };
+            Piece *current_piece = NULL;
+            if (!verify_square(board, san, from, &current_piece)) continue;
 
-            // iterate through all squares on the board
-            Position from = { .row = row, .col = col };
-            
-            // find valid piece at current square
-            Piece *selected_piece = get_piece_at(board, from);
-            if (!selected_piece || selected_piece->type != san.piece) continue;
+            // generate moves
+            PositionList moves = {0};
+            if (!generate_legal_moves(board, from, &moves)) continue;
 
-            // promotion moves imply the piece is a pawn
-            if (san.is_promotion && selected_piece->type != TYPE_PAWN) continue;
+            //? i dont know what im doing anymore
+            Move temp = {0};
+            // update rest of temp
+            temp.piece = san.piece;
+            temp.from = from;
+            // temp.to is decided in validate_moves()
+            temp.is_capture = san.is_capture;
+            temp.is_castle_kingside = san.is_castle_kingside;
+            temp.is_castle_queenside = san.is_castle_queenside;
+            // temp.is_promotion and temp.promotion is decided in validate_move()
 
-            // check if the selected piece can go to the SAN's square
-            PositionList move_list = {0}; // set count=0 and moves to be 0
-            if (!generate_legal_moves(board, from, &move_list)) continue;
+            // validate the moves list of the current piece
+            ResolveStatus piece_status = validate_moves(board, san, current_piece, moves, &temp);
 
-            // check if the SAN's move is in the list of all possible moves
-            for (int i = 0; i < move_list.count; i++)
-            {
-                // select destination square
-                Position to = move_list.moves[i];
+            if (piece_status == RESOLVE_ILLEGAL) continue;
 
-                // if there's a match, process it
-                if (san.to.row == to.row && san.to.col == to.col)
-                {
-                    // handle captures
-                    if (san.is_capture)
-                    {
-                        // don't process move if there is no valid piece (enemy) to capture
-                        Piece *enemy = get_piece_at(board, to);
-                        if (!enemy) continue;
-                    }
+            // return early if ambiguous has been declared already
+            if (status == RESOLVE_AMBIGUOUS) return RESOLVE_AMBIGUOUS;
 
-                    // handle promotions (NOTE: they have lower priority than captures)
-                    if (san.is_promotion || is_pawn_promotion_rank(selected_piece, to))
-                    {
-                        move_out->is_promotion = true;
-                        move_out->promotion = (san.promotion == TYPE_NONE) ? TYPE_QUEEN : san.promotion; // default to queens in the case of implicit SAN promotions
-                        if (san.promotion == TYPE_NONE) puts("none");
-                        if (san.promotion == TYPE_PAWN) puts("pawn");
-                    }
+            if (found) return RESOLVE_AMBIGUOUS;
 
-                    // if the status has previously been marked as OK, then there must be more than one piece that can access this move
-                    if (current_status == RESOLVE_OK) return RESOLVE_AMBIGUOUS;
-                    
-                    // approve status and update move's source square
-                    current_status = RESOLVE_OK;
-                    move_out->from = from;
-                    continue;
-                }
-            }
+            found_move = temp;
+            found = true;
+            status = RESOLVE_OK;
         }
     }
-    move_out->piece = san.piece;
-    move_out->to = san.to;
 
-    return current_status;
+    // finally, link the temp Move struct to the output Move struct
+    if (found) *move_out = found_move;
+
+    return status;
 }
