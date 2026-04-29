90c8b0691ac6cf6ec09b58088fcc682fa6c5ff80
William Pimentel <williamencabopimentel@gmail.com>
2026-04-29 07:32:37 +0100

    Update API to use game state

--- include/fen_parser.h
+++ include/fen_parser.h
@@ -1,22 +1,10 @@
 #ifndef FEN_PARSER_H
 #define FEN_PARSER_H
+
 #include <stdbool.h>
 #include <ctype.h>
-#include "board.h"
-#include "piece.h"
-#include "position.h"
+#include "game_state.h"
 
-typedef struct
-{
-    Board board;
-    bool white_to_move;
-    bool has_en_passant;
-    Position en_passant;
-    int halfmove_clock;
-    int fullmove_number;
-} FenMeta;
-
-
-bool load_fen(Board *board, const char *fen, FenMeta *meta_out, int *err_pos);
+bool load_fen(GameState *game, const char *fen, int *err_pos);
 
 #endif
