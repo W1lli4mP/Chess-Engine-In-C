ca57810f0188ff16008daddf7f0f0fce907a5739
William Pimentel <williamencabopimentel@gmail.com>
2026-04-29 07:44:43 +0100

    Remove rules.h dependency

--- include/move_gen.h
+++ include/move_gen.h
@@ -1,10 +1,10 @@
 #ifndef MOVE_GEN_H
 #define MOVE_GEN_H
+
+#include <stdbool.h>
 #include "position.h"
 #include "piece.h"
 #include "board.h"
-#include "rules.h"
-#include <stdbool.h>
 
 bool generate_pseudo_legal_moves(const Board *board, Position piece_location, PositionList *position_list_out);
 
