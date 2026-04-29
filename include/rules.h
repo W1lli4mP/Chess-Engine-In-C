3d9bd0b5f3689ad9f2697c9b3110883402622b72
William Pimentel <williamencabopimentel@gmail.com>
2026-04-29 06:23:58 +0100

    Update and clean API to use game state

--- include/rules.h
+++ include/rules.h
@@ -1,15 +1,17 @@
 #ifndef RULES_H
 #define RULES_H
-#include "position.h"
-#include "piece.h"
-#include "board.h"
-#include "move_gen.h"
-#include "position.h"
-#include <stdbool.h>
 
-bool is_square_attacked(const Board *board, Position square, Colour friendly_colour);
-bool is_checkmate(const Board *board, Colour colour);
-bool is_stalemate(const Board *board, Colour colour);
-bool generate_legal_moves(const Board *board, Position piece_location, PositionList *position_list_out);
+#include <stdbool.h>
+#include "game_state.h"
+#include "position.h"
+#include "move_gen.h"
+
+bool is_square_attacked(const GameState *game, Position square, Colour friendly_colour);
+bool is_in_check(const GameState *game, Colour colour);
+
+bool is_checkmate(const GameState *game, Colour colour);
+bool is_stalemate(const GameState *game, Colour colour);
+
+bool generate_legal_moves(const GameState *game, Position piece_location, PositionList *position_list_out);
 
 #endif
