bde1c2ec6de27df0d04c9c866b2aba18594af7f3
William Pimentel <williamencabopimentel@gmail.com>
2026-05-06 23:29:30 +0100

    Initial commit

---
+++ include/move_apply.h
@@ -0,0 +1,30 @@
+#ifndef MOVE_APPLY_H
+#define MOVE_APPLY_H
+
+#include <stdbool.h>
+#include "game_state.h"
+#include "move.h"
+#include "position.h"
+
+typedef struct
+{
+    Piece *captured_piece;
+    Position captured_position;
+
+    Colour previous_side_to_move;
+
+    CastlingRights previous_castling_rights;
+
+    bool previous_has_en_passant_target;
+    Position previous_en_passant_target;
+
+    int previous_halfmove_clock;
+    int previous_fullmove_number;
+
+    PieceType previous_moved_piece_type;
+} UndoInfo;
+
+bool make_move(GameState *game, Move move, UndoInfo *undo_out);
+bool unmake_move(GameState *game, Move move, const UndoInfo *undo);
+
+#endif
