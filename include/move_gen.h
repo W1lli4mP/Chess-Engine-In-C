5923a5fa6e7b3bc51bae10b2886080da99139e5a
William Pimentel <williamencabopimentel@gmail.com>
2026-05-06 11:07:44 +0100

    Migrate legal move generation from rules.h to move_gen.h

--- include/move_gen.h
+++ include/move_gen.h
@@ -2,10 +2,13 @@
 #define MOVE_GEN_H
 
 #include <stdbool.h>
+#include "game_state.h"
 #include "position.h"
 #include "piece.h"
 #include "board.h"
 
 bool generate_pseudo_legal_moves(const Board *board, Position piece_location, PositionList *position_list_out);
+
+bool generate_legal_moves(const GameState *game, Position piece_location, PositionList *position_list_out);
 
 #endif
--- include/rules.h
+++ include/rules.h
@@ -12,6 +12,4 @@ bool is_in_check(const GameState *game, Colour colour);
 bool is_checkmate(const GameState *game, Colour colour);
 bool is_stalemate(const GameState *game, Colour colour);
 
-bool generate_legal_moves(const GameState *game, Position piece_location, PositionList *position_list_out);
-
 #endif
