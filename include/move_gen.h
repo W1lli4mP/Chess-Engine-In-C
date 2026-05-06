41b951d49f509b0286924d6de99a85c2cea75b81
William Pimentel <williamencabopimentel@gmail.com>
2026-05-06 11:12:24 +0100

    Add vertical alignment

--- include/move_gen.h
+++ include/move_gen.h
@@ -7,8 +7,15 @@
 #include "piece.h"
 #include "board.h"
 
-bool generate_pseudo_legal_moves(const Board *board, Position piece_location, PositionList *position_list_out);
+bool generate_pseudo_legal_moves(
+    const Board *board,
+    Position piece_location,
+    PositionList *position_list_out
+);
 
-bool generate_legal_moves(const GameState *game, Position piece_location, PositionList *position_list_out);
+bool generate_legal_moves(const GameState *game,
+    Position piece_location,
+    PositionList *position_list_out
+);
 
 #endif
