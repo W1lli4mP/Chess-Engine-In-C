149774ec2dec6801e52088a29df4f96d178cabdb
William Pimentel <williamencabopimentel@gmail.com>
2026-05-06 10:46:42 +0100

    Update API with MoveList + Move creation + MoveList appending

--- include/move.h
+++ include/move.h
@@ -7,18 +7,31 @@
 typedef struct
 {
     PieceType piece;
+
     Position from;
     Position to;
+    
     bool is_capture;
 
     // castling
     bool is_castle_kingside;
     bool is_castle_queenside;
 
+    bool is_en_passant;
+
     // promotion
     bool is_promotion;
     PieceType promotion;
 } Move;
+
+typedef struct
+{
+    Move moves[MAX_MOVES];
+    int count;
+} MoveList;
+
+Move create_move(PieceType piece, Position from, Position to);
+bool move_list_append(MoveList *move_list, Move move);
 
 Move *initialise_move();
 bool destroy_move(Move *move);
