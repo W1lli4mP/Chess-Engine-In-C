d46cc44ea2442f6126d00e470c9effed703f68da
William Pimentel <williamencabopimentel@gmail.com>
2026-05-10 00:25:59 +0100

    Initial commit

---
+++ include/game_play.h
@@ -0,0 +1,11 @@
+#ifndef GAME_PLAY_H
+#define GAME_PLAY_H
+
+#include <stdbool.h>
+
+#include "game_state.h"
+#include "move.h"
+
+bool play_move(GameState *game, Move move);
+
+#endif
---
+++ src/game_play.c
@@ -0,0 +1,13 @@
+#include "game_play.h"
+#include "move_apply.h"
+
+bool play_move(GameState *game, Move move)
+{
+    UndoInfo undo;
+    if (!make_move(game, move, &undo))
+        return false;
+    
+    // append to position history when implemented
+
+    return true;
+}
