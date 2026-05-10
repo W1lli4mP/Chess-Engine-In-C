10efa9ad2220751e17caef94ebd89d08bc23c9a1
William Pimentel <williamencabopimentel@gmail.com>
2026-05-10 02:54:36 +0100

    Fix unconditional declaration of engine debug print

--- include/debug_print.h
+++ include/debug_print.h
@@ -16,8 +16,6 @@ void debug_print_board(const Board *board, int white_pov);
 void debug_print_move(Move move);
 void debug_print_san(San san);
 void debug_print_resolve_status(ResolveStatus status);
-void debug_print_engine_move_score(Move move, int score);
-void debug_print_engine_best_move(Move move, int score);
 
 // macro for engine debug prints
 #ifdef ENGINE_DEBUG
