ad8958cfc6b284f49a52abbdd98531782806cdc5
William Pimentel <williamencabopimentel@gmail.com>
2026-05-10 19:42:38 +0100

    Migrate position key struct to position_key_types + Update API with public position key helper

--- include/position_key.h
+++ include/position_key.h
@@ -4,17 +4,9 @@
 #include <stdbool.h>
 
 #include "game_state.h"
-
-#define POSITION_KEY_LEN 128
-
-// compact form of a chess position to be used as a "lookup key"
-// useful for threefold and fetching transpositions
-typedef struct
-{
-    char text[POSITION_KEY_LEN];
-} PositionKey;
+#include "position_key_types.h"
 
 bool create_position_key(const GameState *game, PositionKey *key_out);
-bool position_keys_equal(const PositionKey *a, PositionKey *b);
+bool position_keys_equal(const PositionKey *a, const PositionKey *b);
 
 #endif
